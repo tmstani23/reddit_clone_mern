61e9f78ce98302063bd561ce5f4aa0267eb8d27b
Tim Work Laptop <tmstani23@gmail.com>
2019-10-25 15:33:01 -0500

    Update 3
    Added post route for user registration.  The registration checks if the user already exists in the db and if not it creates a new user and attaches the registration form fields to the user model.  It then hashes the input password and adds the hashed password back into the user object.
    Added passport.js file that contains passport strategy and collects payload from the registration page.  It then decodes the web token payload.  The payload user id is verified against the users in the db.  If the user exists and is verified the user is returned.

---
+++ config/passport.js
@@ -0,0 +1,31 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+const opts = {};
+//extract the web token from the header and save into the opts object
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+//Add secret key to the opts object
+opts.secretOrKey = process.env.secretOrKey;
+
+//export strategy as passport object
+module.exports = passport => {
+    passport.use(
+        //create new jwt strategy and search db for user
+        new JwtStrategy(opts, (jwt_payload, done) => {
+            //jwt_payload is an object literal containing the decoded JWT payload.
+            //search the db for a user with the same id as the one contained in the payload
+            User.findById(jwt_payload.id)
+                .then(user => {
+                    //if the user id matches return the user
+                    if (user) {
+                        //done is a passport error first callback accepting arguments done(error, user, info)
+                        return done(null, user);
+                    }
+                    return done(null, false);
+                })
+                .catch(err => console.log(err));
+        })
+    );
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -10,3 +10,54 @@ const validateLoginInput = require("../../validation/login");// Load User model
 
 //Load User model
 const User = require("../../models/User");
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+
+//post route for the register page
+router.post("/register", (req, res) => {
+    //Form validation
+    //Pass the register form body to the validateRegisterInput function
+        //and save the resulting objects as variables
+    const {errors, isValid} = validateRegisterInput(req.body);
+    //check if register form inputs are valid
+    if (!isValid) {
+        //if invalid return errors as json
+        return res.status(400).json(errors);
+    }
+    //Search User email fields for existing email
+    User.findOne({email: req.body.email})
+        .then(user => {
+            //if email already exists return email exists response
+            if (user) {
+                return res.status(400).json({email: "Email already exists"});
+            }
+            //Else create a new user and update all the required fields from the form body
+            else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                 //Hash password before saving to db
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            //return the new user as a response
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        
+        })
+
+})
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
