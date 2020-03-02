1bfba9701fa8b15e5e9046606f83c5f2f5f5e2bc
Tim Work Laptop <tmstani23@gmail.com>
2020-03-02 14:54:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ const path = require('path');
 //Initialize mongoose connection with cloud db server
 //Connect to MongoDB
 
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }, function(err) {
+mongoose.connect(process.env.DATABASE_URL || testDB, { useNewUrlParser: true }, function(err) {
   
-//   if(err) {
-//     console.log(err);
+  if(err) {
+    console.log(err);
     
-//   }
-//   //Log if connection was established or not
-//   console.log(mongoose.connection.readyState, "Mongo DB connection established");
-// });
+  }
+  //Log if connection was established or not
+  console.log(mongoose.connection.readyState, "Mongo DB connection established");
+});
 
 
 //Clear database of all connections and reset
@@ -67,14 +67,14 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //Helper functions
 
 
-function clearDatabase(database){
-  mongoose.connect(database, function(err) {
-    if(err) {
-      console.log(err);
-    }
-    //Log if connection was established or not
-    console.log(mongoose.connection.readyState, "Mongo DB connection established");
-    //Clear all collections from database.
-    mongoose.connection.db.dropDatabase();
-  });
-}
+// function clearDatabase(database){
+//   mongoose.connect(database, function(err) {
+//     if(err) {
+//       console.log(err);
+//     }
+//     //Log if connection was established or not
+//     console.log(mongoose.connection.readyState, "Mongo DB connection established");
+//     //Clear all collections from database.
+//     mongoose.connection.db.dropDatabase();
+//   });
+// }
